9e3a3e4217d80e0094526c421f892c2c9f2d6c15
kelly-narine <76927158+kelly-narine@users.noreply.github.com>
2024-06-02 14:32:28 -0400

    Upload DepositUltility.h

---
+++ Workshop07/DepositUltility.h
@@ -0,0 +1,52 @@
+#ifndef SENECA_DEPOSITUTILITY_H
+#define SENECA_DEPOSITUTILITY_H
+
+#include <iostream>
+
+namespace seneca
+{
+	const unsigned int MAX_STRING_LEN = 100;
+	class Date
+	{
+		int m_day{};
+		int m_month{};
+		int m_year{};
+
+	public:
+		Date() = default;
+		Date(int day, int month, int year);
+
+		int getDay() const;
+		int getMonth() const;
+		int getYear() const;
+
+		void write(std::ostream& out) const;
+		void read(std::istream& in, bool interractive = true);
+	};
+
+	class DepositInfo
+	{
+		char m_transit[MAX_STRING_LEN + 1]{};
+		char m_institution[MAX_STRING_LEN + 1]{};
+		char m_account[MAX_STRING_LEN + 1]{};
+
+	public:
+		DepositInfo() = default;
+		DepositInfo(const char* transit, const char* institution, const char* account);
+
+		const char* getTransit() const;
+		const char* getInstitution() const;
+		const char* getAccount() const;
+
+		void write(std::ostream& out) const;
+		void read(std::istream& in, bool interractive = true);
+	};
+
+	std::ostream& operator<<(std::ostream& out, const Date& dt);
+	std::istream& operator>>(std::istream& in, Date& dt);
+
+	std::ostream& operator<<(std::ostream& out, const DepositInfo& info);
+	std::istream& operator>>(std::istream& in, DepositInfo& info);
+}
+
+#endif
