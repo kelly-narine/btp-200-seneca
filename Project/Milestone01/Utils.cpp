b329f009f50f0ce1f31582ea31bace632d98d4c0
kelly-narine <76927158+kelly-narine@users.noreply.github.com>
2024-06-15 12:40:41 -0400

    Upload Utils.cpp

---
+++ Project/Milestone01/Utils.cpp
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdlib>
+#include <ctime>
+#include "Utils.h"
+using namespace std;
+namespace seneca {
+   // start provided code
+   bool debug = false;
+   Utils U;
+   int Utils::getTime() {
+      int mins = -1;
+      if (debug) {
+         int duration[]{ 3,5,9,20,30 };
+         mins = (m_testMins %= 1440);
+         m_testMins += duration[m_testIndex++ % 5];
+      }
+      else {
+         time_t t = time(NULL);
+         tm lt = *localtime(&t);
+         mins = lt.tm_hour * 60 + lt.tm_min;
+      }
+      return mins;
+   }
+   void Utils::setDebugTime(int hour, int min) {
+      seneca::debug = true;
+      m_testMins = hour * 60 + min;
+   }
+   
+}
