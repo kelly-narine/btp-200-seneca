4463aac85178aece9badc3a037e583455211ab90
kelly-narine <76927158+kelly-narine@users.noreply.github.com>
2024-06-26 20:27:09 -0400

    Upload IOAble.cpp

---
+++ Project/Milestone03/IOAble.cpp
@@ -0,0 +1,33 @@
+/* Citation and Sources...
+Final Project Milestone 3
+Module: IOAble
+Filename: IOAble.cpp
+Version 1.0
+Author          Kelly Narine
+
+Revision History
+-----------------------------------------------------------
+Date      Reason
+2024/03/27  Preliminary release
+-----------------------------------------------------------
+*/
+
+#include "IOAble.h"
+
+namespace seneca
+{
+	IOAble::~IOAble() {}
+
+	std::istream& operator>>(std::istream& in, IOAble& ioAble)
+	{
+		ioAble.read(in);
+		return in;
+	}
+
+	std::ostream& operator<<(std::ostream& out, const IOAble& ioAble)
+	{
+		ioAble.write(out);
+		return out;
+	}
+}
+
