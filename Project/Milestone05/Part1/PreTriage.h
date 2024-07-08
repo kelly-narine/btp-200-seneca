3d6e37c511207ac6471be9d054cf4da95f121618
kelly-narine <76927158+kelly-narine@users.noreply.github.com>
2024-07-08 18:57:53 -0400

    Upload PreTriage.h

---
+++ Project/Milestone05/Part1/PreTriage.h
@@ -0,0 +1,45 @@
+/* Citation and Sources...
+Final Project Milestone 5, Part 1
+Module: PreTriage
+Filename: PreTriage.h
+Version 1.0
+Author          Kelly Narine
+
+Revision History
+-----------------------------------------------------------
+Date      Reason
+2024/04/08  Preliminary release
+-----------------------------------------------------------
+*/
+
+#ifndef SENECA_PRETRIAGE_H
+#define SENECA_PRETRIAGE_H
+#include "Time.h"
+#include "Patient.h"
+namespace seneca
+{
+	const int MAX_PATIENTS = 100;
+
+	class PreTriage
+	{
+		Time m_testWaitTime;
+		Time m_triageWaitTime;
+		Patient* m_patientLineup[MAX_PATIENTS];
+		char* m_fileName;
+		int m_numPatients;
+		//Time getWaitTime(const Patient&) const;
+		//void setAverageWaitTime(Patient&);
+		//int indexOfFirstInLine(char type) const;
+		void load();
+		void save() const;
+		void registerPatient();
+		void admit();
+		void lineup() const;
+
+	public:
+		PreTriage(const char* fileName);
+		~PreTriage();
+		void run();
+	};
+}
+#endif
