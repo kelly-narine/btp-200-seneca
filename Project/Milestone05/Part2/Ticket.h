bccdad9c27b581866e77f86de9c517fe18c05425
kelly-narine <76927158+kelly-narine@users.noreply.github.com>
2024-07-12 19:30:00 -0400

    Upload Ticket.h

---
+++ Project/Milestone05/Part2/Ticket.h
@@ -0,0 +1,33 @@
+/* Citation and Sources...
+Final Project Milestone 5, Part 2
+Module: Ticket
+Filename: Ticket.h
+Version 1.0
+Author          Kelly Narine
+
+Revision History
+-----------------------------------------------------------
+Date      Reason
+2024/03/31  Preliminary release
+-----------------------------------------------------------
+*/
+
+#ifndef SENECA_TICKET_H_
+#define SENECA_TICKET_H_
+#include <iostream>
+#include "Time.h"
+#include "IOAble.h"
+namespace seneca {
+   class Ticket:public IOAble{
+      Time m_time;
+      int m_number;
+   public:
+      Ticket(int number);
+      Time time()const;
+      int number()const;
+      void resetTime();
+      std::ostream& write(std::ostream& ostr )const;
+      std::istream& read(std::istream& istr);
+   };
+}
+#endif // !SENECA_TICKET_H_
